acaa3d5f3a67d91a3714dd2246d3483e5f65cca4
hygnic <hygnic@outlook.com>
2022-01-18 17:25:36 +0800

    20200118

---
+++ script/test20220118.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              test20220118
+# Author:            Hygnic
+# Created on:        2022/1/18 15:10
+# Version:           
+# Reference:         
+"""
+Description:         测试 arcpy3 arcgis pro
+Usage:               
+"""
+# -------------------------------------------
+import arcpy
+
+
